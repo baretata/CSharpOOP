73eb221ba7ded18b674c41576a2cbe673e852cd8
baretata <zlatkoatanasov92@gmail.com>
2015-03-21 23:34:43 +0200

    Homeworks

---
+++ 05.Object-Oriented-Programming-Principles-Part-II/01.Shapes/Circle.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.Shapes
+{
+    using System;
+
+    public class Circle : Shape
+    {
+        public Circle(double diameter)
+            : base(diameter, diameter)
+        { 
+            
+        }
+
+        public override double CalculateSurface()
+        {
+            return Math.PI * (this.Width / 2) * (this.Height / 2);
+        }
+    }
+}
